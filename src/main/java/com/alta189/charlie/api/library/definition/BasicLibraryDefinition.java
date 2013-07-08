d0ad66b616a9725f849b733fbcbfa618dc117c9d
Stephen Williams <me@alta189.com>
2013-07-07 20:35:51 -0400

    Basic Library Definition Files Reader needs to be implemented

---
+++ src/main/java/com/alta189/charlie/api/library/definition/BasicLibraryDefinition.java
@@ -0,0 +1,59 @@
+/*
+ * This file is part of Charlie CI API.
+ *
+ * Copyright (c) ${project.inceptionYear}, Stephen Williams (alta189) <http://charlie.alta189.com/>
+ * Charlie CI API is licensed under the GNU Lesser General Public License.
+ *
+ * Charlie CI API is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Charlie CI API is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.alta189.charlie.api.library.definition;
+
+public class BasicLibraryDefinition {
+	private String name;
+	private String version;
+	private String url;
+	private String md5;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getMd5() {
+		return md5;
+	}
+
+	public void setMd5(String md5) {
+		this.md5 = md5;
+	}
+}
